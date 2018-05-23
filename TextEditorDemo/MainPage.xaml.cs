4905d5882d9044259709d796fff481599c52fe63
Taras Sobchyshak <taras.sobchyshak@Blackthorn-vision.com>
2018-05-23 15:16:43 +0300

    editor added

--- TextEditorDemo/MainPage.xaml.cs
+++ TextEditorDemo/MainPage.xaml.cs
@@ -13,18 +13,20 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace TextEditorDemo
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var function = @"(function () {return $('div#froala-editor').froalaEditor('html.get'); })()";
+            var html = await EditorView.InvokeScriptAsync("eval", new string[] { function });
+            EditorHtmlTextBox.Text = html;
         }
     }
 }
